d68907cc429cbe8968164e1a496db364e0af0a92
fathcode99 <fath970106@gmail.com>
2022-08-18 14:31:49 +0700

    detailpages, params

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import Axios from 'axios'
 import { useParams } from 'react-router-dom'
 
+import {
+    Container, Col, Row
+} from "react-bootstrap"
 
+import "./stylePages.css"
 
 export default function Detail() {
     const [products, setProducts] = useState({})
@@ -10,15 +14,29 @@ export default function Detail() {
     let { id } = useParams();
     useEffect(() => {
         Axios.get(`http://localhost:2000/products/${id}`)
-        .then(res => {
-            console.log(res.data)
-            setProducts(res.data)
-        })
+            .then(res => {
+                console.log(res.data)
+                setProducts(res.data)
+            })
     }, [id])
 
     return (
-        <div style={{ fontSize: "100px" }}>
-            {products.name}
-        </div>
+        <Container fluid className="primary-container">
+            <Row className="container-detail">
+                <Col>
+                    {products.images ?
+                        <img src={products.images[0]} alt="products"/>
+                        : []}
+                </Col>
+
+                <Col>
+                    <Col className="detail-title">{products.name}</Col>
+                    <Col className="detail-p">{products.description}</Col>
+                    <Col className="detail-stock">BRAND : {products.brand}</Col>
+                    <Col className="detail-stock">STOCK : {products.stock}</Col>
+                    <Col className="detail-stock">HARGA : {products.price}</Col>
+                </Col>
+            </Row>
+        </Container>
     )
 }
